2e088df9981b8d8e2d7f760b263c2cb62df5ecea
Adrien <adriennavarre@hotmail.com>
2022-05-06 22:07:22 -0400

    progress on user auth

---
+++ routes/users.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const router = require('exress').Router();
+const User = mongoose.model('User');
+const passport = require('passport');
+const utils = require('../utils');
+
+router.get('/admin', (req, res, next) =>{
+
+})
+
+router.post('login', function(req, res, next){
+    User.findOne({ username: req.body.username })
+        .then((user) =>{
+            if(!user){
+                res.status(401).json({ success: false, msg: "could not find user" });
+            }
+
+            const isValid = utils.validPassword(req.body.password, user.hash, user.salt);
+
+            if(isValid){
+                const tokenObject = utils.issueJWT(user)
+
+                res.status(200).json({ success: true, user: user, token: tokenObject.token, expiresIn: tokenObject.expires });
+            } else{
+                res.status(401).json({ success: false, msg: "you entered the wrong password"});
+            }
+        })
+        .catch((err) => next(err));
+})
+
+router.post('/register', function(req, res, next){
+    const saltHash = utils.genPassword(req.body.password);
+
+    const salt = saltHash.salt;
+    const hash = saltHash.hash;
+
+    const newUser = new User({
+        username: req.body.username,
+        hash: hash,
+        salt: salt
+    });
+
+    newUser.save()
+        .then((user) =>{
+            const jwt = utils.issueJWT(user)
+
+            res.json({ success: true, user: user, token: jwt.token, expiresIn: jwt.expires });
+        })
+})
+
+module.exports = router;
---
+++ utils/index.js
@@ -0,0 +1,45 @@
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const path = require('path');
+
+const pathToKey = //finish later
+const PRIV_KEY = //finish later
+
+
+function validPassword(password, hash, salt) {
+    var hashVerify = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('hex');
+    return hash === hashVerify;
+}
+
+function genPassword(password) {
+    var salt = crypto.randomBytes(32).toString('hex');
+    var genHash = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('hex');
+
+    return{
+        salt: salt,
+        hash: genHash
+    };
+}
+
+function issueJWT(user) {
+    const _id = user._id;
+
+    const expiresIn = '7d';
+
+    const payload = {
+        sub: _id,
+        iat: Date.now()
+    };
+
+    const signedToken = jwt.sign(payload, PRIV_KEY, {expiresIn: expiresIn, algorithm: 'RS256' });
+
+    return {
+        token: "Bearer " + signedToken,
+        expires: expiresIn
+    }
+}
+    
+module.exports.validPassword = validPassword;
+module.exports.genPassword = genPassword;
+module.exports.issueJWT = issueJWT;
