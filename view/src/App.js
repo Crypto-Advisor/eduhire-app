796af248089de0fe8b6c8dd719a9edb5cee0a298
Adrien <adriennavarre@hotmail.com>
2022-05-08 23:37:38 -0700

    setup rough frontend files

--- view/src/App.js
+++ view/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
+import Home from './pages/home'
+import Login from './pages/login'
+import Apply from './pages/apply'
+import Applicants from './pages/applicants'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      
+      <Routes>
+        <Route exact path ='/' element={<Home />} />
+        <Route path ='/login' element={<Login />} />
+        <Route path ='/apply' element={<Apply />} />
+        <Route path ='/apply' element={<Applicants />} />
+      </Routes>
+    </Router>
   );
 }
 
