ce274d46ae59d99a62d562cf30ad547ded092a96
Adrien <adriennavarre@hotmail.com>
2022-05-16 23:03:02 -0700

    implemented axios on view utils

--- view/src/utils/forms.js
+++ view/src/utils/forms.js
@@ -1,29 +1,31 @@
+import axios from 'axios'
+
+axios.defaults.headers.common = {
+    "Content-Type": "application/json"
+}
+
+const baseUrl = '/forms'
+
 export const fetchForms = async () =>{
-    const res = await fetch('http://localhost:3000/forms');
+    const res = await axios.get(baseUrl)
+        .catch(err => console.log(err));
     return res;
 }
 
 export const createForm = async (data) => {
-    const res = await fetch('http://localhost:3000/forms/add', {
-        method: 'PUT',
-        body: data
-    })
-    .catch(err => console.log(err))
+    const res = await axios.put(`${baseUrl}/add`, data)
+        .catch(err => console.log(err));
     return res;
 }
 
 export const updateForm = async (data) => {
-    const res = await fetch('/forms/update', {
-        method: 'POST',
-        body: data
-    });
+    const res = await axios.post(`${baseUrl}/update`, data)
+        .catch(err => console.log(err));
     return res;
 }
 
 export const removeForm = async (data) => {
-    const res = await fetch('/forms/delete', {
-        method: 'DELETE',
-        body: data
-    });
+    const res = await axios.delete(`${baseUrl}/delete`, data)
+        .catch(err => console.log(err));
     return res;
 }
--- view/src/utils/users.js
+++ view/src/utils/users.js
@@ -1,22 +1,25 @@
+import axios from 'axios'
+
+axios.defaults.headers.common = {
+    "Content-Type": "application/json"
+}
+
+const baseUrl = '/users'
+
 export const login = async (data) =>{
-    const res = await fetch('http://localhost:3000/users/login', {
-        method: 'POST',
-        body: data
-    })
-    .catch(err => console.log(err))
+    const res = await axios.post(`${baseUrl}/login`, data)
+        .catch(err => console.log(err));
     return res;
 }
 
 export const register = async (data) => {
-    const res = await fetch('http://localhost:3000/users/register', {
-        method: 'POST',
-        body: data
-    })
-    .catch(err => console.log(err))
+    const res = await axios.post(`${baseUrl}/register`, data)
+        .catch(err => console.log(err));
     return res;
 }
 
 export const admin = async () => {
-    const res = await fetch('/admin');
+    const res = await axios.get(`${baseUrl}/admin`)
+        .catch(err => console.log(err));
     return res;
 }
